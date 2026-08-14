01d1f7b037306a122ea7dcfdfb9b5e4d3438e3d5
AiDev168 <iosdevbook@gmail.com>
2026-08-14 13:22:30 +0430

    test: avoid coupling GUI contract to helper names

--- tests/unit/test_gui_contract.py
+++ tests/unit/test_gui_contract.py
@@ -37,6 +37,5 @@ def test_dashboard_has_request_console_live_refresh_and_llm_settings() -> None:
         "in_process",
         "function viewSession",
         "Promise.allSettled",
-        "loadSessionPart",
     ):
         assert text in html
